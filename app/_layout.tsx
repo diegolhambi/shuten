a0e2dad2678c3f62cf9ee72290144e843f885672
Diego Lhambi Pereira <diegolhambipereira@gmail.com>
2023-09-17 20:22:47 -0300

    style: Change provider to providers

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,7 +8,7 @@ import { SplashScreen, Stack } from 'expo-router';
 import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
 
-import { Provider } from '../provider/provider';
+import { Provider } from '../providers/provider';
 
 export {
     // Catch any errors thrown by the Layout component.
