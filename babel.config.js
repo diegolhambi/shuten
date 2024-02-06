6e8cd5497dd7ef787edf4d8bb92bcdb81b34c0c0
Diego Lhambi Pereira <diegolhambipereira@gmail.com>
2024-02-05 21:49:44 -0300

    feat: add tamagui compiler

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,19 @@ module.exports = (api) => {
     api.cache(true);
     return {
         presets: ['babel-preset-expo'],
+        plugins: [
+            [
+                '@tamagui/babel-plugin',
+                {
+                    components: ['tamagui'],
+                    config: './tamagui.config.ts',
+                    importsWhitelist: ['constants.js', 'colors.js'],
+                    logTimings: true,
+                    disableExtraction: process.env.NODE_ENV === 'development',
+                    experimentalFlattenThemesOnNative: true,
+                }
+            ],
+
+        ]
     };
 };
