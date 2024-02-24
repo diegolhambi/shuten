d428fe3bcd712807c90c6b777eec4b76b92e3d0b
Diego Lhambi Pereira <diegolhambipereira@gmail.com>
2024-02-24 17:10:08 -0300

    feat: add random notification messages

--- providers/notification-manager.tsx
+++ providers/notification-manager.tsx
@@ -2,85 +2,12 @@ import * as Notifications from 'expo-notifications';
 import { DateTime } from 'luxon';
 import React, { createContext, useEffect, useMemo, useState } from 'react';
 
-import { Punch } from '@/types/punch';
+import { Punches, usePunchStore } from '@/providers/punches';
+import { Punch, PunchType } from '@/types/punch';
 import { Weekday } from '@/utils/date';
+import { predictDailyPunches } from '@/utils/punch-list';
+import { DeviceEventEmitter } from 'react-native';
 import { useConfig } from './config';
-import { Punches } from '@/providers/punches';
-
-type NotificationManagerContextData = {
-    granted: boolean | undefined;
-    requestPermission: () => Promise<boolean>;
-    scheduleFirstPunch: (punches: Punches) => Promise<void>;
-    scheduleNext: (dateTime: DateTime, punch: Punch[]) => void;
-};
-
-const NotificationContext = createContext<NotificationManagerContextData>(
-    {} as NotificationManagerContextData
-);
-
-type Props = {
-    children?: React.ReactNode;
-};
-
-async function checkPermission() {
-    const settings = await Notifications.getPermissionsAsync();
-
-    if (
-        settings.ios?.status &&
-        settings.ios.status >= Notifications.IosAuthorizationStatus.AUTHORIZED
-    ) {
-        return true;
-    }
-
-    return settings.granted;
-}
-
-function registerChannels() {
-    Notifications.setNotificationCategoryAsync('punch', [
-        {
-            identifier: 'punch-now',
-            buttonTitle: 'Punch',
-        },
-    ]);
-}
-
-const textNotications = {
-    0: {
-        title: 'Time to start work',
-        body: "It's time to start your workday. Please clock in now to begin your shift.",
-    },
-    1: {
-        title: 'Lunchtime',
-        body: "It's time for your lunchtime. Don't forget to clock out!",
-    },
-    2: {
-        title: 'Return from lunch',
-        body: "Welcome back! Don't forget to clock in to resume your work.",
-    },
-    3: {
-        title: 'End of work',
-        body: 'Congratulations, your workday is over! Remember to clock out.',
-    },
-};
-
-const textEarlyNotifications = {
-    0: {
-        title: 'Get ready to start work',
-        body: 'Your workday will begin in % minutes.',
-    },
-    1: {
-        title: 'Lunch break reminder',
-        body: 'Your lunch break is in % minutes.',
-    },
-    2: {
-        title: 'Returning to work soon',
-        body: 'You will be returning from lunch in % minutes.',
-    },
-    3: {
-        title: 'End of workday reminder',
-        body: 'Your workday will end in % minutes.',
-    },
-};
 
 export function NotificationProvider({ children }: Props) {
     const { config } = useConfig();
@@ -92,19 +19,6 @@ export function NotificationProvider({ children }: Props) {
         checkPermission().then((result) => {
             setGranted(result);
         });
-    }, []);
-
-    useEffect(() => {
-        const subscription =
-            Notifications.addNotificationResponseReceivedListener(
-                (response) => {
-                    console.log(response);
-                }
-            );
-
-        return () => {
-            subscription.remove();
-        };
     }, []);
 
     async function requestPermission() {
@@ -124,11 +38,16 @@ export function NotificationProvider({ children }: Props) {
                 minutes: config.notification.howEarly,
             });
 
+            const message =
+                textEarlyNotifications[punch][
+                    Math.random() * (textEarlyNotifications[punch].length - 1)
+                ];
+
             Notifications.scheduleNotificationAsync({
                 content: {
                     categoryIdentifier: 'punch',
-                    title: textEarlyNotifications[punch].title,
-                    body: textEarlyNotifications[punch].body.replace(
+                    title: message.title,
+                    body: message.body.replace(
                         '%',
                         `${config.notification.howEarly}`
                     ),
@@ -144,7 +63,9 @@ export function NotificationProvider({ children }: Props) {
             Notifications.scheduleNotificationAsync({
                 content: {
                     categoryIdentifier: 'punch',
-                    ...textNotications[punch],
+                    ...textNotications[punch][
+                        Math.random() * (textNotications[punch].length - 1)
+                    ],
                 },
                 identifier: `punch_${punch}_${time.toFormat(
                     'yyyy-MM-dd_HH-mm'
@@ -278,3 +199,227 @@ export function NotificationProvider({ children }: Props) {
 export function useNotification() {
     return React.useContext(NotificationContext);
 }
+
+type NotificationManagerContextData = {
+    granted: boolean | undefined;
+    requestPermission: () => Promise<boolean>;
+    scheduleFirstPunch: (punches: Punches) => Promise<void>;
+    scheduleNext: (dateTime: DateTime, punch: Punch[]) => void;
+};
+
+const NotificationContext = createContext<NotificationManagerContextData>(
+    {} as NotificationManagerContextData
+);
+
+type Props = {
+    children?: React.ReactNode;
+};
+
+async function checkPermission() {
+    const settings = await Notifications.getPermissionsAsync();
+
+    if (
+        settings.ios?.status &&
+        settings.ios.status >= Notifications.IosAuthorizationStatus.AUTHORIZED
+    ) {
+        return true;
+    }
+
+    return settings.granted;
+}
+
+function registerChannels() {
+    Notifications.setNotificationCategoryAsync('punch', [
+        {
+            identifier: 'punch-now',
+            buttonTitle: 'Punch',
+        },
+    ]);
+}
+
+type TextNotification = {
+    [key in 0 | 1 | 2 | 3]: {
+        title: string;
+        body: string;
+    }[];
+};
+
+const textNotications: TextNotification = {
+    0: [
+        {
+            title: 'Time to start work',
+            body: "It's time to start your workday. Please clock in now to begin your shift.",
+        },
+        { title: 'Workday begins!', body: "Let's conquer the day!" },
+        { title: 'Time to shine!', body: 'Clock in and get productive.' },
+        { title: 'Your journey starts!', body: 'Start your day with a win.' },
+        { title: 'Good morning!', body: 'Time to kick off your workday! ' },
+        {
+            title: "Let's get down to business!",
+            body: 'Clock in and conquer your goals.',
+        },
+        {
+            title: 'Rise and shine!',
+            body: 'A new day, new opportunities. Seize them!',
+        },
+    ],
+    1: [
+        {
+            title: 'Lunchtime',
+            body: "It's time for your lunchtime. Don't forget to clock out!",
+        },
+        { title: 'Lunch break!', body: 'Recharge and refuel!' },
+        { title: 'Time to eat!', body: "Don't forget to clock out." },
+        { title: 'Midday break!', body: 'Enjoy your well-deserved rest.' },
+        {
+            title: 'Lunchtime is calling!',
+            body: 'Fuel up and recharge for the afternoon.',
+        },
+        {
+            title: 'Treat yourself to a break!',
+            body: "It's time to step away and refresh.",
+        },
+        {
+            title: 'Time to refuel!',
+            body: "Don't forget to clock out and enjoy your lunch!",
+        },
+    ],
+    2: [
+        {
+            title: 'Return from lunch',
+            body: "Welcome back! Don't forget to clock in to resume your work.",
+        },
+        { title: 'Back to work!', body: "Let's finish strong!" },
+        { title: 'Time to focus!', body: 'Clock in and power through.' },
+        { title: 'Final push!', body: 'Almost there, keep going!' },
+        {
+            title: 'Back to work!',
+            body: "Let's make the most of the remaining hours.",
+        },
+        {
+            title: 'Final stretch!',
+            body: 'Finish strong and achieve your targets!',
+        },
+        {
+            title: 'Time to power through!',
+            body: 'Clock in and give it your all!',
+        },
+    ],
+    3: [
+        {
+            title: 'End of work',
+            body: 'Congratulations, your workday is over! Remember to clock out.',
+        },
+        { title: 'Workday done!', body: 'Time to relax and unwind!' },
+        { title: 'You did it!', body: 'Clock out and enjoy your time.' },
+        { title: 'Cheers to freedom!', body: 'Your workday is over!' },
+        { title: 'Congratulations!', body: "You've completed your workday! " },
+        {
+            title: 'Time to clock out!',
+            body: 'Enjoy your well-deserved rest and relaxation.',
+        },
+        {
+            title: 'Freedom at last!',
+            body: 'Go forth and conquer your free time!',
+        },
+    ],
+};
+
+const textEarlyNotifications: TextNotification = {
+    0: [
+        {
+            title: 'Get ready to start work',
+            body: 'Your workday will begin in % minutes.',
+        },
+        { title: 'Work starts soon!', body: 'Get ready in % mins.' },
+        {
+            title: 'Almost time to work!',
+            body: 'Prepare for a productive day.',
+        },
+        { title: 'Your workday awaits!', body: 'Clock in in % mins.' },
+        {
+            title: 'Heads up!',
+            body: 'Your workday starts in % mins. Get ready!',
+        },
+        {
+            title: 'The clock is ticking!',
+            body: 'Prepare for a productive day in % mins.',
+        },
+        {
+            title: 'Your journey begins soon!',
+            body: 'Get set for your workday in % mins.',
+        },
+    ],
+    1: [
+        {
+            title: 'Lunch break reminder',
+            body: 'Your lunch break is in % minutes.',
+        },
+        { title: 'Lunch break in % mins!', body: 'Pack your delicious meal.' },
+        { title: 'Get ready to refuel!', body: 'Lunchtime is almost here.' },
+        {
+            title: 'Food break coming up!',
+            body: "Don't forget your lunch in % mins.",
+        },
+        {
+            title: 'Lunch break alert!',
+            body: "It's almost time to refuel in % mins.",
+        },
+        {
+            title: 'Get ready to eat!',
+            body: 'Pack your lunch and prepare for a break in % mins.',
+        },
+        {
+            title: 'Food break approaching!',
+            body: "Don't forget your delicious meal in % mins.",
+        },
+    ],
+    2: [
+        {
+            title: 'Returning to work soon',
+            body: 'You will be returning from lunch in % minutes.',
+        },
+        { title: 'Back from lunch in % mins!', body: 'Make the most of it!' },
+        { title: 'Time to resume work!', body: 'Clock in after your break.' },
+        { title: 'Lunch break is over!', body: 'Back to work in % mins.' },
+        {
+            title: 'Time to return!',
+            body: 'Your lunch break ends in % mins. Clock in soon!',
+        },
+        {
+            title: 'Back to work reminder!',
+            body: 'Get ready to resume your tasks in % mins.',
+        },
+        {
+            title: 'Break over!',
+            body: "It's time to clock in and continue in % mins.",
+        },
+    ],
+    3: [
+        {
+            title: 'End of workday reminder',
+            body: 'Your workday will end in % minutes.',
+        },
+        { title: 'Work ends in % mins!', body: 'Wrap it up and relax!' },
+        {
+            title: 'Almost done for the day!',
+            body: 'Finish those tasks in % mins.',
+        },
+        {
+            title: 'Your workday is almost over!',
+            body: 'Get ready to enjoy your free time in % mins.',
+        },
+        {
+            title: 'Almost there!',
+            body: 'Your workday ends in % mins. Finish strong!',
+        },
+        {
+            title: 'Wrapping it up!',
+            body: 'Get those last tasks done in % mins.',
+        },
+        {
+            title: 'Time to celebrate!',
+            body: 'Your workday is almost over in % mins.',
+        },
+    ],
+};
